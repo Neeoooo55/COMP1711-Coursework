778833b07f277468ca18751ef80004c3fa3e4ae4
Frederick Pugh <146678628+Neeoooo55@users.noreply.github.com>
2023-10-27 20:26:30 +0000

    Changes

--- StepsTask1.c
+++ StepsTask1.c
@@ -11,19 +11,20 @@ typedef struct {
 
 // Define any additional variables here
 
-
+FITNESS_DATA FITNESS_DATA_ARRAY[500];
+int count = 0;
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
-void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps) {
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL) {        
+        strcpy(date, token);
     }
     
     token = strtok(NULL, delimiter);
@@ -38,25 +39,33 @@ void tokeniseRecord(const char *input, const char *delimiter,
     
     // Free the duplicated string
     free(inputCopy);
+}
 
+FILE *open_file(char *filename, char *mode)
+{
+    FILE *file = fopen(filename, mode);
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
+    return file;
 }
 
 // Complete the main function
 int main() {
     char* filename = "FitnessData_2023.csv";
-    FILE *file = fopen(filename, "r");
-
-    if (file == NULL) {
-        printf("Error opening file\n");
-        return 1;
-    }
+    FILE *file = open_file(filename, "r");
 
     int buffer_size = 100;
     char line_buffer[buffer_size];
 
     while (fgets(line_buffer, buffer_size, file) != NULL) {
         printf("%s\n", line_buffer);
+        count += 1;
     }
+
+    printf("Number of records in file: %d\n", count);
 
     fclose(file);
     return 0;
